c2c86fe535ec1d18bb15a8a1fbbefdf53872669c
wegissilveira <wegis.h.silveira@gmail.com>
2023-01-18 19:39:40 -0300

    feat(general): install sass and get the first points that need improvement

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import classes from "./Footer.module.scss";
 
-import classes from './Footer.module.css'
+const Footer = () => {
+   return (
+      <div className={classes["Footer-container"]}>
+         <p>
+            Desenvolvido por{" "}
+            <a href="https://www.wegis.com.br">Wegis Silveira</a>
+         </p>
+      </div>
+   );
+};
 
-const Footer: React.FC = () => {
-    return (
-        <div className={classes['Footer-container']}>
-            <p>Desenvolvido por <a href="https://www.wegis.com.br">Wegis Silveira</a></p>
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -1,21 +1,24 @@
-import React, { Component } from 'react'
+import Classes from './Layout.module.scss'
 
-import Navigation from 'components/Navigation/Navigation'
-import Footer from 'components/Footer/Footer'
+import Navigation from "components/Navigation/Navigation"
+import Footer from "components/Footer/Footer"
 
+type Props = {
+   children: React.ReactNode
+}
 
-class Layout extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Navigation />
-                <main>
-                    { this.props.children }
-                </main>
-                <Footer />
-            </React.Fragment>
-        )
-    }
+const Layout = (props: Props) => {
+   const {
+      children
+   } = props
+
+   return (
+      <div className={Classes['Main-wrapper']}>
+         <Navigation />
+         <main>{children}</main>
+         <Footer />
+      </div>
+   )
 }
 
 export default Layout
